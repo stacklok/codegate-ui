afa5e116a35a594794099e53967e1d0d8d896c33
Giuseppe Scuglia <peppescg@gmail.com>
2025-01-03 18:54:26 +0100

    test: add PromptList

--- src/mocks/msw/handlers.ts
+++ src/mocks/msw/handlers.ts
@@ -1,12 +1,12 @@
 import { http, HttpResponse } from "msw";
+import mockedPrompts from "@/mocks/msw/fixtures/GetMessages.json";
+import mockedAlerts from "@/mocks/msw/fixtures/GetAlerts.json";
 
 export const handlers = [
-  http.get("https://example.com/user", () => {
-    // ...and respond to them using this JSON response.
-    return HttpResponse.json({
-      id: "c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d",
-      firstName: "John",
-      lastName: "Maverick",
-    });
+  http.get("*/dashboard/messages", () => {
+    return HttpResponse.json(mockedPrompts);
+  }),
+  http.get("*/dashboard/alerts", () => {
+    return HttpResponse.json(mockedAlerts);
   }),
 ];
