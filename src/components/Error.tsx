7ed6b759baafc00570b2ee533f77ca187549b1c7
Giuseppe Scuglia <peppescg@gmail.com>
2025-01-15 10:59:51 +0100

    feat: replace icon

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -1,4 +1,4 @@
-import { XCircle } from "lucide-react";
+import { CircleAlert } from "lucide-react";
 import { Header } from "./Header";
 import { Card } from "./ui/card";
 
@@ -10,11 +10,11 @@ export function Error() {
       </div>
       <div className="h-24 flex flex-col flex-1 justify-center">
         <Card className="p-8 flex flex-col items-center">
-          <XCircle className="text-red-600 mb-2 size-8" />
-          <div className="text-md font-semibold text-gray-600 text-center">
+          <CircleAlert className="text-red-600 mb-2 size-16" />
+          <div className="text-xl font-semibold text-gray-600 text-center">
             An error occurred
           </div>
-          <div className="text-sm text-gray-600 text-center text-balance">
+          <div className="text-md mb-4 text-gray-600 text-center text-balance">
             If this issue persists, please reach out to us on{" "}
             <a
               className="underline text-gray-700"
