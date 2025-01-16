cc7d14913685633a8e3252d606ba8c82cec1f48f
Daniel Kantor <kd@stacklok.com>
2025-01-16 13:28:39 +0100

    fix failing tests

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -7,23 +7,12 @@ import { ChatMessageList } from "./ui/chat/chat-message-list";
 import { useParams } from "react-router-dom";
 import { usePromptsData } from "@/hooks/usePromptsData";
 import { Markdown } from "./Markdown";
-import { useEffect } from "react";
 import { sanitizeQuestionPrompt } from "@/lib/utils";
-import { useCurrentPromptStore } from "@/hooks/useCurrentPromptStore";
 
 export function Chat() {
   const { id } = useParams();
   const { data: prompts } = usePromptsData();
-  const { setCurrentPromptId } = useCurrentPromptStore();
   const chat = prompts?.find((prompt) => prompt.chat_id === id);
-
-  useEffect(() => {
-    if (id) {
-      setCurrentPromptId(id);
-    }
-
-    return () => setCurrentPromptId("");
-  }, [prompts, id, setCurrentPromptId]);
 
   return (
     <div className="w-[calc(100vw-18rem)]">
