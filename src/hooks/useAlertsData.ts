0ad120e69752bfbb289c1fbc27ea72b2f95a58a8
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2025-01-17 10:30:36 +0000

    feat: react-query hey-api integration

--- src/hooks/useAlertsData.ts
+++ src/hooks/useAlertsData.ts
@@ -4,6 +4,7 @@ import { AlertConversation } from "@/api/generated";
 import { getMaliciousPackage } from "@/lib/utils";
 import { MaliciousPkgType, TriggerType } from "@/types";
 import { useAlertSearch } from "./useAlertSearch";
+import { getAlertsDashboardAlertsGetQueryKey } from "@/api/generated/@tanstack/react-query.gen";
 
 const fetchAlerts = async (): Promise<AlertConversation[]> => {
   const { getAlertsDashboardAlertsGet } = await serverApi();
@@ -25,7 +26,7 @@ const fetchAlerts = async (): Promise<AlertConversation[]> => {
 
 export const useAlertsData = ({ ...args } = {}) => {
   return useQuery({
-    queryKey: ["alerts"],
+    queryKey: getAlertsDashboardAlertsGetQueryKey(),
     queryFn: fetchAlerts,
     ...args,
   });
