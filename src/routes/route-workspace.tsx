991b1ae3d88a5b2452f8aa3b9828fceb2a9f54cc
Daniel Kantor <kd@stacklok.com>
2025-01-20 15:17:22 +0100

    add breadcrumbs to edit workspace page

--- src/routes/route-workspace.tsx
+++ src/routes/route-workspace.tsx
@@ -1,10 +1,16 @@
 import { SystemPromptEditor } from "@/features/workspace-system-prompt/components/system-prompt-editor";
 import { WorkspaceName } from "@/features/workspace/components/workspace-name";
-import { Heading } from "@stacklok/ui-kit";
+import { Breadcrumb, Breadcrumbs, Heading } from "@stacklok/ui-kit";
 
 export function RouteWorkspace() {
   return (
     <>
+      <Breadcrumbs>
+        <Breadcrumb href="/">Dashboard</Breadcrumb>
+        <Breadcrumb href="/workspaces">Manage Workspaces</Breadcrumb>
+        <Breadcrumb>Edit Workspace</Breadcrumb>
+      </Breadcrumbs>
+
       <Heading level={1}>Workspace settings</Heading>
       <WorkspaceName className="mb-4" />
       <SystemPromptEditor className="mb-4" />
