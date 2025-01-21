1126f1715df30bd9124a7a7597fdc3d432aaff52
Daniel Kantor <kd@stacklok.com>
2025-01-21 15:35:27 +0100

    remove react import from automatically generated icon components

--- src/components/icons/Continue.tsx
+++ src/components/icons/Continue.tsx
@@ -1,4 +1,3 @@
-import * as React from "react";
 import type { SVGProps } from "react";
 const SvgContinue = (props: SVGProps<SVGSVGElement>) => (
   <svg
--- src/components/icons/Youtube.tsx
+++ src/components/icons/Youtube.tsx
@@ -1,4 +1,3 @@
-import * as React from "react";
 import type { SVGProps } from "react";
 const SvgYoutube = (props: SVGProps<SVGSVGElement>) => (
   <svg
