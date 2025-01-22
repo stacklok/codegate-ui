44d167394675708b6214033ffdfedc0088f7cdbe
Giuseppe Scuglia <peppescg@gmail.com>
2025-01-22 18:19:47 +0100

    fix: parsing promptList text and breadcrumb

--- src/components/__tests__/PromptList.test.tsx
+++ src/components/__tests__/PromptList.test.tsx
@@ -5,10 +5,51 @@ import mockedPrompts from "@/mocks/msw/fixtures/GET_MESSAGES.json";
 import { render } from "@/lib/test-utils";
 import { Conversation } from "@/api/generated";
 
+const conversationTimestamp = "2025-01-02T14:19:58.024100Z";
 const prompt = mockedPrompts[0] as Conversation;
 
+const testCases: [string, { message: string; expected: RegExp | string }][] = [
+  [
+    "codegate cmd",
+    {
+      message: "codegate workspace -h",
+      expected: /codegate workspace -h/i,
+    },
+  ],
+  [
+    "render code with path",
+    {
+      message: "// Path: src/lib/utils.ts",
+      expected: /Prompt on filepath: src\/lib\/utils.ts/i,
+    },
+  ],
+  [
+    "render code with file path",
+    {
+      message: "<file> ```tsx // filepath: /tests/my-test.tsx import",
+      expected: /Prompt on file\/\/ filepath: \/tests\/my-test.tsx/i,
+    },
+  ],
+  [
+    "render snippet",
+    {
+      message:
+        'Compare this snippet from src/test.ts: // import { fakePkg } from "fake-pkg";',
+      expected: /Prompt from snippet compare this snippet from src\/test.ts:/i,
+    },
+  ],
+  [
+    "render fallback",
+    {
+      message:
+        "<file> ```Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      expected: "Prompt 2025/01/03 - 10:09:33 AM",
+    },
+  ],
+];
+
 describe("PromptList", () => {
-  it("should render correct prompt", () => {
+  it("render prompt", () => {
     render(<PromptList prompts={[prompt]} />);
     expect(
       screen.getByRole("link", {
@@ -17,17 +58,26 @@ describe("PromptList", () => {
     ).toBeVisible();
   });
 
-  it("should render default prompt value when missing question", async () => {
-    const conversationTimestamp = "2025-01-02T14:19:58.024100Z";
+  it.each(testCases)("%s", (_title: string, { message, expected }) => {
     render(
       <PromptList
         prompts={[
           {
-            question_answers: [],
-            provider: "vllm",
-            type: "fim",
-            chat_id: "b97fbe59-0e34-4b98-8f2f-41332ebc059a",
-            conversation_timestamp: conversationTimestamp,
+            ...prompt,
+            question_answers: [
+              {
+                answer: {
+                  message: "Mock AI answer",
+                  message_id: "fake_ai_id",
+                  timestamp: conversationTimestamp,
+                },
+                question: {
+                  message,
+                  message_id: "fake_id",
+                  timestamp: conversationTimestamp,
+                },
+              },
+            ],
           },
         ]}
       />,
@@ -35,7 +85,7 @@ describe("PromptList", () => {
 
     expect(
       screen.getByRole("link", {
-        name: `Prompt ${conversationTimestamp}`,
+        name: expected,
       }),
     ).toBeVisible();
   });
