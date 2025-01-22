e7d0b02aa8eb95c0386d760184d17e83e23ec475
Giuseppe Scuglia <peppescg@gmail.com>
2025-01-22 18:27:59 +0100

    test: check only the day

--- src/components/__tests__/PromptList.test.tsx
+++ src/components/__tests__/PromptList.test.tsx
@@ -43,7 +43,7 @@ const testCases: [string, { message: string; expected: RegExp | string }][] = [
     {
       message:
         "<file> ```Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      expected: "Prompt 2025/01/03 - 10:09:33 AM",
+      expected: /Prompt 2025\/01\/03/i,
     },
   ],
 ];
