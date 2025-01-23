f2cf621f7f486323aabb14b9a9d83325d21e69a9
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2025-01-23 10:54:36 +0000

    feat: useKbdShortcuts hook & example implementation

---
+++ src/hooks/use-kbd-shortcuts.ts
@@ -0,0 +1,31 @@
+import { useEffect } from "react";
+
+export function useKbdShortcuts(map: [string, () => void][]) {
+  return useEffect(() => {
+    // Attach a listener to the document to listen for the "/" key
+    const documentListener = (e: KeyboardEvent) => {
+      const target = e.target as HTMLElement;
+      if (
+        target.tagName === "INPUT" ||
+        target.tagName === "TEXTAREA" ||
+        target.isContentEditable
+      ) {
+        return;
+      }
+
+      for (const [key, callback] of map) {
+        if (e.key.toLowerCase() === key.toLowerCase()) {
+          e.preventDefault();
+          e.stopPropagation();
+          callback();
+        }
+      }
+    };
+
+    document.addEventListener("keydown", documentListener);
+
+    return () => {
+      document.removeEventListener("keydown", documentListener);
+    };
+  }, [map]);
+}
