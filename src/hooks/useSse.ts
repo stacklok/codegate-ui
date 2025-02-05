697509e14f3ae194b5b0e7964993415a1fcfae25
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2025-02-05 09:24:56 +0000

    feat: more granular invalidation of data when alerts SSE is received

--- src/hooks/useSse.ts
+++ src/hooks/useSse.ts
@@ -1,6 +1,11 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import { useQueryClient } from "@tanstack/react-query";
+import { Query, useQueryClient } from "@tanstack/react-query";
+import { OpenApiTsReactQueryKey } from "@/types/openapi-ts";
+import {
+  v1GetWorkspaceAlertsQueryKey,
+  v1GetWorkspaceMessagesQueryKey,
+} from "@/api/generated/@tanstack/react-query.gen";
 
 const BASE_URL = import.meta.env.VITE_BASE_API_URL;
 
@@ -15,7 +20,20 @@ export function useSse() {
 
     eventSource.onmessage = function (event) {
       if (event.data.toLowerCase().includes("new alert detected")) {
-        queryClient.invalidateQueries({ refetchType: "all" });
+        queryClient.invalidateQueries({
+          refetchType: "all",
+          predicate: (
+            query: Query<unknown, Error, unknown, OpenApiTsReactQueryKey>,
+          ) =>
+            query.queryKey[0]._id ===
+              v1GetWorkspaceAlertsQueryKey({
+                path: { workspace_name: "default" }, // NOTE: Just supplying "default" to satisfy the type-checker, because we are just using the `_id`, this invalidates for any workspace
+              })[0]?._id ||
+            query.queryKey[0]._id ===
+              v1GetWorkspaceMessagesQueryKey({
+                path: { workspace_name: "default" }, // NOTE: Just supplying "default" to satisfy the type-checker, because we are just using the `_id`, this invalidates for any workspace
+              })[0]?._id,
+        });
       }
     };
 
