0ae1d16d3903575a41d7b9d23de27d253956021d
Giuseppe Scuglia <peppescg@gmail.com>
2025-02-13 11:09:54 +0100

    refactor: simplify logic for handling dirty and reset form state

--- src/hooks/useFormState.ts
+++ src/hooks/useFormState.ts
@@ -1,5 +1,5 @@
-import { isEqual } from 'lodash'
-import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
+import { isEqual } from "lodash";
+import { useCallback, useMemo, useRef, useState } from "react";
 
 export type FormState<T> = {
   values: T
@@ -19,34 +19,28 @@ function useDeepMemo<T>(value: T): T {
 export function useFormState<Values extends Record<string, unknown>>(
   initialValues: Values
 ): FormState<Values> {
-  const memoizedInitialValues = useDeepMemo(initialValues)
-
+  const memoizedInitialValues = useDeepMemo(initialValues);
   // this could be replaced with some form library later
-  const [values, setValues] = useState<Values>(memoizedInitialValues)
-  const [originalValues, setOriginalValues] = useState<Values>(values)
+  const [values, setValues] = useState<Values>(memoizedInitialValues);
 
-  useEffect(() => {
-    // this logic supports the use case when the initialValues change
-    // due to an async request for instance
-    setOriginalValues(memoizedInitialValues)
-    setValues(memoizedInitialValues)
-  }, [memoizedInitialValues])
-
-  const updateFormValues = useCallback((newState: Partial<Values>) => {
-    setValues((prevState: Values) => ({
-      ...prevState,
-      ...newState,
-    }))
-  }, [])
+  const updateFormValues = useCallback(
+    (newState: Partial<Values>) => {
+      setValues((prevState: Values) => ({
+        ...prevState,
+        ...newState,
+      }));
+    },
+    [setValues],
+  );
 
   const resetForm = useCallback(() => {
-    setValues(originalValues)
-  }, [originalValues])
+    setValues(memoizedInitialValues);
+  }, [memoizedInitialValues]);
 
   const isDirty = useMemo(
-    () => !isEqual(values, originalValues),
-    [values, originalValues]
-  )
+    () => !isEqual(values, initialValues),
+    [values, initialValues],
+  );
 
   const formState = useMemo(
     () => ({ values, updateFormValues, resetForm, isDirty }),
