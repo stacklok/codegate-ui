f00e9d1e20d74cafb1bf352ab2aa6cff930c80a4
alex-mcgovern <alex_mcgovernsmith@icloud.com>
2025-02-14 16:16:19 +0000

    fix tests

--- src/markdown/certificates/linux-remove.md
+++ src/markdown/certificates/linux-remove.md
@@ -1,2 +1,5 @@
-1. Run `certutil -d sql:$HOME/.pki/nssdb -D -n CodeGate-CA`
+1. Run the following command to uninstall the certificate from your account:
+    ```shell
+    certutil -d sql:$HOME/.pki/nssdb -D -n CodeGate-CA
+    ```
 2. Restart VS Code.
--- src/routes/__tests__/route-certificates.test.tsx
+++ src/routes/__tests__/route-certificates.test.tsx
@@ -19,7 +19,7 @@ describe('Certificates', () => {
     )
 
     expect(
-      screen.getByRole('heading', { name: 'Certificate Management' })
+      screen.getByRole('heading', { name: 'Certificate management' })
     ).toBeVisible()
 
     expect(screen.getByText('macOS')).toBeVisible()
@@ -42,18 +42,26 @@ describe('Certificates', () => {
     render(<RouteCertificates />)
 
     expect(screen.getByRole('heading', { name: 'CLI method' })).toBeVisible()
-    expect(screen.getByText('After downloading the certificate')).toBeVisible()
+    expect(
+      screen.getByText('After downloading the certificate', { exact: false })
+    ).toBeVisible()
     expect(screen.getByRole('heading', { name: 'GUI method' })).toBeVisible()
-    expect(screen.getByText('Open the downloaded certificate')).toBeVisible()
+    expect(
+      screen.getByText('Open the downloaded certificate', { exact: false })
+    ).toBeVisible()
 
     await userEvent.click(
       screen.getByRole('button', { name: 'Remove certificate' })
     )
 
     expect(screen.getByRole('heading', { name: 'CLI method' })).toBeVisible()
-    expect(screen.getByText('Open a terminal and run')).toBeVisible()
+    expect(
+      screen.getByText('Open a terminal and run', { exact: false })
+    ).toBeVisible()
     expect(screen.getByRole('heading', { name: 'GUI method' })).toBeVisible()
-    expect(screen.getByText('Launch the Keychain Access app')).toBeVisible()
+    expect(
+      screen.getByText('Launch the Keychain Access app', { exact: false })
+    ).toBeVisible()
   })
 
   it('should render Windows certificate installation', async () => {
@@ -77,12 +85,18 @@ describe('Certificates', () => {
     await userEvent.click(screen.getByText('Linux'))
 
     expect(
-      screen.getByText('/usr/local/share/ca-certificates/codegate.crt')
+      screen.getByText(
+        'Run the following command to install the certificate for your account:'
+      )
     ).toBeVisible()
 
     await userEvent.click(
       screen.getByRole('button', { name: 'Remove certificate' })
     )
-    expect(screen.getByText('/usr/local/share/ca-certificates/')).toBeVisible()
+    expect(
+      screen.getByText(
+        'Run the following command to uninstall the certificate from your account:'
+      )
+    ).toBeVisible()
   })
 })
