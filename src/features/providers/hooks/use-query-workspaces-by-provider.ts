f3bf4987544946c801d6a6fb4dfbdad486a5f21b
Giuseppe Scuglia <peppescg@gmail.com>
2025-02-20 09:23:45 +0100

    chore: update openapi

---
+++ src/features/providers/hooks/use-query-workspaces-by-provider.ts
@@ -0,0 +1,14 @@
+import { v1ListWorkspacesByProviderOptions } from '@/api/generated/@tanstack/react-query.gen'
+import { useQuery } from '@tanstack/react-query'
+
+export function useQueryWorkspacesByProvider(
+  providerId: string | undefined | null
+) {
+  if (!providerId) {
+    throw new Error('providerId is required')
+  }
+
+  return useQuery({
+    ...v1ListWorkspacesByProviderOptions({ path: { provider_id: providerId } }),
+  })
+}
